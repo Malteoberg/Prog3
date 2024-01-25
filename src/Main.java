c837feaf4ca2a551c7cb69df6bc6477110c0c8a5
Benjamin Makela <benjamin.makela@yh.nackademin.se>
2024-01-25 10:57:58 +0100

    Fixa strängar på output

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
-        // Create instances of the classes
+        // Skapa instanser av klasserna
         Current current_obj = new Current();
         History history_obj = new History();
         Forecast forecast_obj = new Forecast();
 
-        String[] countries = {"Current weather", "Weather Forecast", "Weather history specific date"};
-        String selectedCountry = (String) JOptionPane.showInputDialog(null, "Select a country:", "Weather Information",
-                JOptionPane.QUESTION_MESSAGE, null, countries, countries[0]);
+        String[] options = {"Current weather", "Weather Forecast", "Weather history specific date"};
+        String selectedCountry = (String) JOptionPane.showInputDialog(null, "Select a option:", "Weather Information",
+                JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 
 
         if (selectedCountry == "Weather Forecast") {
